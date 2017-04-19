06587d4fac4f701e9d4dac844bdd8e04e8cacbc2
Kiran Rao <Kiran Rao>
2017-04-19 18:29:34 +0530

    Initial commit

---
+++ app/src/main/java/ahk/wito/com/wi_to/Constants.java
@@ -0,0 +1,14 @@
+package ahk.wito.com.wi_to;
+
+/**
+ * Created by admin on 2/20/2017.
+ */
+
+public class Constants {
+    //public static final String SERVER_IP = "10.60.42.87";
+    public static final int SERVER_PORT = 1234;
+    public static final String MOUSE_RIGHT_CLICK="right_click";
+    public static final String MOUSE_LEFT_CLICK="left_click";
+
+}
+
---
+++ app/src/main/java/ahk/wito/com/wi_to/MainActivity.java
@@ -0,0 +1,197 @@
+package ahk.wito.com.wi_to;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //public static final String ABC = null;
+
+    public String ABC;
+
+    Context context;
+    Button LeftButton;
+    Button RightButton,kbtn;
+    TextView mousePad;
+    EditText kText;
+
+    private boolean isConnected=false;
+    private boolean mouseMoved=false;
+    private Socket socket;
+    private PrintWriter out;
+
+    private float initX =0;
+    private float initY =0;
+    private float disX =0;
+    private float disY =0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        kText = (EditText)findViewById(R.id.ipbox);
+
+        context = this; //save the context to show Toast messages
+
+        //Get references of all buttons
+        LeftButton = (Button)findViewById(R.id.LeftButton);
+        RightButton = (Button)findViewById(R.id.RightButton);
+        kbtn = (Button)findViewById(R.id.kbtn);
+
+
+        //this activity extends View.OnClickListener, set this as onClickListener
+        //for all buttons
+        LeftButton.setOnClickListener(this);
+        RightButton.setOnClickListener(this);
+        kbtn.setOnClickListener(this);
+
+        //Get reference to the TextView acting as mousepad
+        mousePad = (TextView)findViewById(R.id.mousePad);
+
+        //capture finger taps and movement on the textview
+        mousePad.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(isConnected && out!=null){
+                    switch(event.getAction()){
+                        case MotionEvent.ACTION_DOWN:
+                            //save X and Y positions when user touches the TextView
+                            initX =event.getX();
+                            initY =event.getY();
+                            mouseMoved=false;
+                            break;
+                        case MotionEvent.ACTION_MOVE:
+                            disX = event.getX()- initX; //Mouse movement in x direction
+                            disY = event.getY()- initY; //Mouse movement in y direction
+                            /*set init to new position so that continuous mouse movement
+                            is captured*/
+                            initX = event.getX();
+                            initY = event.getY();
+                            if(disX !=0|| disY !=0){
+                                out.println(disX +","+ disY); //send mouse movement to server
+                            }
+                            mouseMoved=true;
+                            break;
+                        case MotionEvent.ACTION_UP:
+                            //consider a tap only if usr did not move mouse after ACTION_DOWN
+                            if(!mouseMoved){
+                                out.println(Constants.MOUSE_LEFT_CLICK);
+                            }
+                    }
+                }
+                return true;
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        ABC = kText.getText().toString();
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if(id == R.id.action_connect) {
+            ConnectPhoneTask connectPhoneTask = new ConnectPhoneTask();
+            connectPhoneTask.execute(ABC); //try to connect to server in another thread
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //OnClick method is called when any of the buttons are pressed
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.LeftButton:
+                if (isConnected && out!=null) {
+                    out.println(Constants.MOUSE_LEFT_CLICK);//send "left_click" to server
+                }
+                break;
+            case R.id.RightButton:
+                if (isConnected && out!=null) {
+                    out.println(Constants.MOUSE_RIGHT_CLICK); //send "right_click" to server
+                }
+                break;
+            case R.id.kbtn:
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+                break;
+        }
+
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        if(isConnected && out!=null) {
+            try {
+                out.println("exit"); //tell server to exit
+                socket.close(); //close socket
+            } catch (IOException e) {
+                Log.e("remotedroid", "Error in closing socket", e);
+            }
+        }
+    }
+
+    public class ConnectPhoneTask extends AsyncTask<String,Void,Boolean> {
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            boolean result = true;
+            try {
+                InetAddress serverAddr = InetAddress.getByName(params[0]);
+                socket = new Socket(serverAddr, Constants.SERVER_PORT);//Open socket on server IP and port
+            } catch (IOException e) {
+                Log.e("remotedroid", "Error while connecting1", e);
+                result = false;
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result)
+        {
+            isConnected = result;
+            Toast.makeText(context,isConnected?"Connected to server!":"Error while connecting2",Toast.LENGTH_LONG).show();
+            try {
+                if(isConnected) {
+                    out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket
+                            .getOutputStream())), true); //create output stream to send data to server
+                }
+            }catch (IOException e){
+                Log.e("remotedroid", "Error while creating OutWriter", e);
+                Toast.makeText(context,"Error while connecting3",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
